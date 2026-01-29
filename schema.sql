7a7eba5c306461b441d7f65f5eac666146be5003
Elmo Eskelinen <elmo.k.eskelinen@gmail.com>
2026-01-29 16:28:32 +0200

    Add new item form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    book_name TEXT,
+    author TEXT,
+    grade FLOAT,
+    review TEXT,
+    user_id INTEGER REFERENCES users
+);
