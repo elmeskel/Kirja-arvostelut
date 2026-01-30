10ed763991709c57a96abc89aaaae3bb6105ad70
Elmo Eskelinen <elmo.k.eskelinen@gmail.com>
2026-01-30 11:25:03 +0200

    Add edit item

--- items.py
+++ items.py
@@ -1,7 +1,6 @@
 import db
 
 def add_item(book_name, author, grade, review, user_id):
-    
     sql = """INSERT INTO items (book_name, author, grade, review, user_id) 
             VALUES (?, ?, ?, ?, ?)"""
     db.execute(sql, [book_name, author, grade, review, user_id])
@@ -11,12 +10,22 @@ def get_items():
     return db.query(sql)
 
 def get_item(item_id):
-    sql = """SELECT items.book_name,
+    sql = """SELECT items.id,
+                    items.book_name,
                     items.author,
                     items.grade,
                     items.review,
+                    users.id user_id,
                     users.username
             FROM items, users
             WHERE items.user_id = users.id AND
             items.id = ?"""
     return db.query(sql, [item_id])[0]
+
+def update_item(item_id,book_name, author, grade, review):
+    sql = """UPDATE items   SET book_name = ?,
+                                author = ?,
+                                grade = ?,
+                                review = ?
+                            WHERE id == ?"""
+    db.execute(sql, [book_name, author, grade, review, item_id])
