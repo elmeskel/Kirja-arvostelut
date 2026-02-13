b867ff6b01e5779724862e94ffc6e2745606582c
Elmo Eskelinen <elmo.k.eskelinen@gmail.com>
2026-02-13 13:26:08 +0200

    Fix check login

--- users.py
+++ users.py
@@ -19,12 +19,12 @@ def create_user(username, password):
 
 def check_login(username, password):
     sql = "SELECT id, password_hash FROM users WHERE username = ?"
-    result = db.query(sql, [username])[0]
+    result = db.query(sql, [username])
     if not result:
         return None
     
-    user_id = result["id"]
-    password_hash = result["password_hash"]
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
     print(password_hash)
     if check_password_hash(password_hash, password):
         return user_id
